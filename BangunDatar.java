a763555ba2c59d3d0e5b914ea19dd098f0b7d816
Notio21 <95495311+Notio21@users.noreply.github.com>
2022-11-09 16:28:02 +0700

    Menambahkan file BangunDatar

---
+++ BangunDatar.java
@@ -0,0 +1,10 @@
+public class BangunDatar{
+    float luas(){
+        System.out.println(x= "Tampilkan Luas");
+        return 0;
+    }
+    float keliling(){
+        System.out.println(x= "Tampilkan Keliling");
+        return 0;
+    }
+}
