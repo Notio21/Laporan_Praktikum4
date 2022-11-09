05313e9661686cc8ec93b5f4b3f0505f8dcba51a
Notio21 <95495311+Notio21@users.noreply.github.com>
2022-11-09 16:31:50 +0700

    Menambahkan file program Persegi.java

---
+++ Persegi.java
@@ -0,0 +1,16 @@
+public class Persegi extends BangunDatar{
+    
+    private int sisi;
+
+    public Persegi(int sisi){
+        this.sisi = sisi;
+    }
+
+    public float luas(){
+        return  this.sisi * this.sisi;
+    }
+
+    public float keliling(){
+        return this.sisi*4;
+    }
+}
